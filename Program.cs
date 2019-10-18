5fc9476d762dbef011894fdead08ebd2f892fb3c
Shan <Shan@SHAN-PC.com>
2019-10-18 17:38:51 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace myConsoleAppSourceCode
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Current date time :"+DateTime.Now);
         }
     }
 }
